f3e52a3e5be53d8fc6b7f5bdc28a7bb8dfdc6ed6
Tony Heupel <tony@heupel.net>
2014-10-15 13:53:43 -0700

    updating of board works; add generation logic next

--- gol-engine.js
+++ gol-engine.js
@@ -7,14 +7,68 @@ gol.engine = {
 
   gameLoopInterval: null,
 
-  start: function(board) {
-    var processGeneration = function(board) {
-      console.log("doing work: " + Date.now());
+  start: function(displayBoard, framesPerSecond) {
+    var translateDisplayBoardToWorkingBoard = function(displayBoard) {
+
+      var processRow = function(displayBoardRow) {
+        var processColumn = function(displayBoardColumn) {
+          return {
+            marked: displayBoardColumn.classList.contains("marked"),
+            displayBoardColumn: displayBoardColumn
+          };
+
+        };
+        var workingRow = [];
+        var displayBoardColumns = displayBoardRow.getElementsByTagName("td");
+
+        for (var columnIndex = 0; columnIndex < displayBoardColumns.length; columnIndex++) {
+          var workingColumn = processColumn(displayBoardColumns[columnIndex]);
+          workingRow.push(workingColumn);
+        }
+        return workingRow;
+      };
+
+      var workingBoard = [];
+
+      var displayBoardRows = displayBoard.getElementsByTagName("tr");
+
+      for (var rowIndex = 0; rowIndex < displayBoardRows.length; rowIndex++) {
+        var workingRow = processRow(displayBoardRows[rowIndex]);
+        workingBoard.push(workingRow);
+      }
+
+      return workingBoard;
     };
 
+
+    var workingBoard = translateDisplayBoardToWorkingBoard(displayBoard);
+
+    var renderInterval = Math.floor(1000/framesPerSecond);
+
+    var processGeneration = function(workingBoard) {
+      console.log("doing work: " + Date.now());
+      workingBoard[5][2].marked = true; // Just test updating marked property
+      console.log(workingBoard);
+    };
+
+    var updateDisplayBoard = function(workingBoard) {
+      var updateColumn = function (workingColumn) {
+        var markFunction = workingColumn.marked ? gol.utility.markCell : gol.utility.clearCell;
+        markFunction(workingColumn.displayBoardColumn);
+      }
+
+      for (var rowIndex = 0; rowIndex < workingBoard.length; rowIndex++) {
+        var workingRow = workingBoard[rowIndex];
+        for (var columnIndex = 0; columnIndex < workingRow.length; columnIndex++) {
+          updateColumn(workingRow[columnIndex]);
+        }
+      }
+    }
+
     this.gameLoopInterval = setInterval(function() {
-      processGeneration(board);
-    }, 16);
+      processGeneration(workingBoard);
+      updateDisplayBoard(workingBoard);
+    }, renderInterval);
 
   },
 
