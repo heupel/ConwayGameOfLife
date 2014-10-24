8fe2cb113be33d88d6c7467cef084747c24d8a65
Tony Heupel <tony@heupel.net>
2014-10-24 13:04:50 -0700

    stuff from yesterday

--- gol-addons.js
+++ gol-addons.js
@@ -70,6 +70,9 @@ gol.addons = {
     document.removeChild(gol.addons.FRAMES_PER_SECOND_CONTAINER_ID);
   },
 
+
+
+
   addFancyTransitions: function () {
     var fancyStyle = document.createElement("style");
     fancyStyle.setAttribute("type", "text/css");
@@ -94,28 +97,133 @@ gol.addons = {
   },
 
   removeFancyTransitions: function () {
-    document.removeChild(gol.addons.FANCY_TRANSITIONS_STYLES_ID);
+    var fancyTransitionsStyle = document.getElementById(gol.addons.FANCY_TRANSITIONS_STYLES_ID);
+    fancyTransitionsStyle.parentNode.removeChild(fancyTransitionsStyle);
   },
 
+
+  addFancyTransitionsControls: function (containerId) {
+    var fancyTransitionsContainer = document.createElement("span");
+    fancyTransitionsContainer.setAttribute("id", "fancyTransitionsContainer");
+
+    var fancyTransitionsLabel = document.createElement("label");
+    fancyTransitionsLabel.setAttribute("for", "fancyTransitions");
+    fancyTransitionsLabel.innerHTML = "Fancy transitions: ";
+
+    var fancyTransitionsCheckbox = document.createElement("input");
+    fancyTransitionsCheckbox.setAttribute("type", "checkbox");
+    fancyTransitionsCheckbox.addEventListener("change", function (ev) {
+      var isChecked = ev.target.checked;
+      if (isChecked) {
+        gol.addons.addFancyTransitions();
+      } else {
+        gol.addons.removeFancyTransitions();
+      }
+    });
+
+    var outerContainer = document.getElementById(containerId);
+    if (typeof(outerContainer) === "undefined" || outerContainer === null) {
+      throw new Error("cannot add the fancy transition controls inside of container: " + containerId);
+    }
+
+    fancyTransitionsContainer.appendChild(fancyTransitionsLabel);
+    fancyTransitionsContainer.appendChild(fancyTransitionsCheckbox);
+
+    outerContainer.appendChild(fancyTransitionsContainer);
+
+  },
+
+
+  trackAgesUpdateCellEventListener: function (ev) {
+      var cell = ev.data;
+      var displayCell = cell.displayBoardColumn;
+      var ageSpans = displayCell.getElementsByClassName("age");
+      var ageSpan;
+
+      if (typeof(ageSpans) === "undefined" || ageSpans.length === 0) {
+        return;
+      }
+      ageSpan = ageSpans[0];
+      ageSpan.innerHTML = cell.generationsLive > 0 ? cell.generationsLive : "";
+  },
+
+
   addTrackAges: function () {
+    var addAgeDisplayElements = function () {
+      var displayCells = document.getElementsByTagName("td");
+      for (var cellIndex = 0; cellIndex < displayCells.length; cellIndex += 1) {
+        var displayCell = displayCells[cellIndex];
+
+        var ageSpan = document.createElement("span");
+        ageSpan.classList.add("age");
+        displayCell.appendChild(ageSpan);
+      }
+    };
+
+    addAgeDisplayElements();
 
     var trackAgesStyle = document.createElement("style");
     trackAgesStyle.setAttribute("type", "text/css");
     trackAgesStyle.setAttribute("id", gol.addons.TRACK_AGES_STYLES_ID);
-    trackAgesStyle.innerHTML = "td { color: #000000; } td.marked { color: #ffffff }";
+    trackAgesStyle.innerHTML = "td .age { color: #ff0000; }";
     document.body.appendChild(trackAgesStyle);
 
-    gol.engine.addEventListener(gol.engine.eventNames.UPDATE_CELL, function(ev) {
-      var cell = ev.data;
-      cell.displayBoardColumn.innerHTML = cell.generationsLive;
-    });
+    gol.engine.addEventListener(gol.engine.eventNames.UPDATE_CELL, gol.addons.trackAgesUpdateCellEventListener);
   },
 
 
   removeTrackAges: function () {
-    gol.engine.removeEventListener(gol.engine.eventNames.UPDATE_CELL, gol.addons.trackAgesUpdateCellEventListener);
-    document.removeChild(gol.addons.TRACK_AGES_STYLES_ID);
-    // TODO: Clear out ages text
-  }
+    var removeAgeDisplayElements = function () {
+      var ages = document.getElementsByClassName("age");
+      if (ages != null) {
+        var numAges = ages.length;
+        var numAgesRemoved = 0;
 
+        for (var ageIndex = 0; ageIndex < ages.length; ageIndex += 1) {
+          var ageElement = ages[ageIndex];
+          //console.log("removing age index: " + ageIndex);
+          ageElement.parentNode.removeChild(ageElement);
+          numAgesRemoved += 1;
+        }
+        console.log("total ages removed: " + numAgesRemoved);
+        //ages = document.getElementsByClassName("age");
+      }
+    };
+
+    removeAgeDisplayElements();
+
+    gol.engine.removeEventListener(gol.engine.eventNames.UPDATE_CELL, gol.addons.trackAgesUpdateCellEventListener);
+    var trackAgesStyle = document.getElementById(gol.addons.TRACK_AGES_STYLES_ID);
+    trackAgesStyle.parentNode.removeChild(trackAgesStyle);
+  },
+
+  addTrackAgesControls: function (containerId) {
+    var trackAgesContainer = document.createElement("span");
+    trackAgesContainer.setAttribute("id", "trackAgesContainer");
+
+    var trackAgesLabel = document.createElement("label");
+    trackAgesLabel.setAttribute("for", "trackAges");
+    trackAgesLabel.innerHTML = "Track ages: ";
+
+    var trackAgesCheckbox = document.createElement("input");
+    trackAgesCheckbox.setAttribute("type", "checkbox");
+    trackAgesCheckbox.addEventListener("change", function (ev) {
+      var isChecked = ev.target.checked;
+      if (isChecked) {
+        gol.addons.addTrackAges();
+      } else {
+        gol.addons.removeTrackAges();
+      }
+    });
+
+    var outerContainer = document.getElementById(containerId);
+    if (typeof(outerContainer) === "undefined" || outerContainer === null) {
+      throw new Error("cannot add the track ages controls inside of container: " + containerId);
+    }
+
+    trackAgesContainer.appendChild(trackAgesLabel);
+    trackAgesContainer.appendChild(trackAgesCheckbox);
+
+    outerContainer.appendChild(trackAgesContainer);
+  }
 };
